c1b6301ade319fa4be644286c5ad917e166153d0
1813114234@qq.com <1813114234@qq.com>
2024-01-19 15:19:09 +0800

    feat: improving Accessibility

--- app/dashboard/invoices/create/page.tsx
+++ app/dashboard/invoices/create/page.tsx
@@ -4,7 +4,6 @@ import { fetchCustomers } from '@/app/lib/data';
 
 export default async function Page() {
   const customers = await fetchCustomers();
-  console.log('customers', customers);
   return (
     <main>
       <Breadcrumbs
